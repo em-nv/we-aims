577ddc2e5b8fa82ee17c9b1241fe345d43d20181
EM <eon2022-8975-60998@bicol-u.edu.ph>
2024-04-23 15:15:50 +0800

    some edits

--- js/customized.js
+++ js/customized.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-/* AUTO CHANGING IMAGE EVERY LOAD OF PAGE */
+/* AUTO CHANGING IMAGE EVERY LOAD OF LOGIN PAGE */
 document.addEventListener("DOMContentLoaded", function() {
     // Array containing URLs of images
     var images = [
@@ -36,6 +36,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Get the img element by its id
     var imgElement = document.getElementById("randomLogInImage");
+
+    // Set the src attribute of the img element to the randomly selected image URL
+    imgElement.src = images[randomIndex];
+});
+
+
+
+/* AUTO CHANGING IMAGE EVERY LOAD OF REGISTER PAGE */
+document.addEventListener("DOMContentLoaded", function() {
+    // Array containing URLs of images
+    var images = [
+        "https://source.unsplash.com/1Ah8CAwk3vM/600x800",
+        "https://source.unsplash.com/U4w7y0lowL8/600x800",
+        "https://source.unsplash.com/thtUUYPdxWY/600x800"
+        // Add more image URLs as needed
+    ];
+
+    // Select a random index from the images array
+    var randomIndex = Math.floor(Math.random() * images.length);
+
+    // Get the img element by its id
+    var imgElement = document.getElementById("randomRegisterImage");
 
     // Set the src attribute of the img element to the randomly selected image URL
     imgElement.src = images[randomIndex];
