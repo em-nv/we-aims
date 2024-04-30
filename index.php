d0c8bfe72d15172b19ac970359075df9b94a2e90
EM <eon2022-8975-60998@bicol-u.edu.ph>
2024-04-30 11:08:58 +0800

    dashboard reports button edit

--- index.php
+++ index.php
@@ -305,8 +305,8 @@
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                        <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> -->
                     </div>
 
 
@@ -331,7 +331,7 @@
                                 <p class="mb-0">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum delectus blanditiis, excepturi perferendis hic impedit eum labore inventore fuga placeat molestiae dolorem aperiam velit ad optio in consequatur illo recusandae.</p>
                                 
                                 <!-- Page Heading -->
-                                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                                <div class="d-sm-flex align-items-center justify-content-between mb-4" style="padding-top:10px;">
                                     <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
                                         <i class="fas fa-solid fa-chart-line fa-sm text-white-50"></i> View Reports</a>
                                 </div>
