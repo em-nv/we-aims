d45c74c86743316862431ae625698d1e2c0ea47d
V7igcv <igcv2022-3306-76073@bicol-u.edu.ph>
2024-05-13 22:10:14 +0800

    finished total profit and notification

--- index.php
+++ index.php
@@ -110,21 +110,39 @@ $formatted_total_sales = number_format($total_sales, 2);
 ?>
 
 <!-- PROFIT COUNT -->
-<?php 
-include 'pages/cus_db.php'; // database connection
+<?php
+include 'pages/cus_db.php';
 
-// Execute SQL query to sum the product sales
-$sql = "SELECT SUM(totalRetailPrice) AS total_productSales FROM transactionspro";
+$sql = "SELECT productName, retailPrice, quantity FROM transactionspro";
 $result = $conn->query($sql);
 
-// Fetch the sum result
+$totalProfit = 0;
+
 if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    $total_product_sales = $row["total_productSales"];
-    $formatted_product_sales = number_format($total_product_sales, 2);
-} else {
-    $total_product_sales = 0;
-    $formatted_product_sales = number_format($total_product_sales, 2);
+    while ($row = $result->fetch_assoc()) {
+        $productName = $row['productName'];
+        $retailPrice = $row['retailPrice'];
+        $quantity = $row['quantity'];
+
+        // Retrieve cost price for this product from products table
+        $costPriceQuery = "SELECT costPrice FROM products WHERE productName = '$productName'";
+        $costPriceResult = $conn->query($costPriceQuery);
+
+        if ($costPriceResult->num_rows > 0) {
+            $costPriceRow = $costPriceResult->fetch_assoc();
+            $costPrice = $costPriceRow['costPrice'];
+
+            // Calculate profit for this transaction
+            $transactionProfit = ($retailPrice - $costPrice) * $quantity;
+
+            // Accumulate total profit
+            $totalProfit += $transactionProfit;
+        } else {
+            echo "an error occured";
+        }
+    }
+
+    $finalProfit = $total_service_sales + $totalProfit;
 }
 
 $conn->close();
@@ -384,49 +402,46 @@ $conn->close();
                             <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-bell fa-fw"></i>
-                                <!-- Counter - Alerts -->
+                                
                                 <span class="badge badge-danger badge-counter">3+</span>
                             </a>
-                            <!-- Dropdown - Alerts -->
+                            
                             <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="alertsDropdown">
                                 <h6 class="dropdown-header">
                                     Alerts Center
                                 </h6>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
+                                <div class="dropdown-item d-flex align-items-center">
                                     <div class="mr-3">
                                         <div class="icon-circle bg-primary">
                                             <i class="fas fa-file-alt text-white"></i>
                                         </div>
                                     </div>
                                     <div>
-                                        <div class="small text-gray-500">December 12, 2019</div>
-                                        <span class="font-weight-bold">A new monthly report is ready to download!</span>
+                                        Hi <?php echo $user_data['fname'];?>, please don't forget to monitor which products are in low supply.
                                     </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
+                                </div>
+                                <div class="dropdown-item d-flex align-items-center">
                                     <div class="mr-3">
                                         <div class="icon-circle bg-success">
                                             <i class="fas fa-donate text-white"></i>
                                         </div>
                                     </div>
                                     <div>
-                                        <div class="small text-gray-500">December 7, 2019</div>
-                                        $290.29 has been deposited into your account!
+                                        New Arrival Alert: LED Headlights. Get 20% off our new LED headlights. Upgrade today!
                                     </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
+                                </div>
+                                <div class="dropdown-item d-flex align-items-center" href="#">
                                     <div class="mr-3">
                                         <div class="icon-circle bg-warning">
                                             <i class="fas fa-exclamation-triangle text-white"></i>
                                         </div>
                                     </div>
                                     <div>
-                                        <div class="small text-gray-500">December 2, 2019</div>
-                                        Spending Alert: We've noticed unusually high spending for your account.
+                                        All brake discs have passed quality checks. Sell with confidence!
                                     </div>
-                                </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                                </div>
+                                
                             </div>
                         </li>
 
@@ -540,7 +555,7 @@ $conn->close();
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                                 PROFIT</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">Php 9,000</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">Php <?php echo number_format($finalProfit, 2); ?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-solid fa-peso-sign fa-2x text-blue-300"></i>
@@ -809,20 +824,7 @@ $conn->close();
                                 <div
                                     class="card-header card-header-alert py-3 d-flex flex-row align-items-center justify-content-between">
                                     <h6 class="m-0 font-weight-bold text-primary">REMINDERS</h6>
-                                    <div class="dropdown no-arrow">
-                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                                            aria-labelledby="dropdownMenuLink">
-                                            <div class="dropdown-header">Dropdown Header:</div>
-                                            <a class="dropdown-item" href="#">Action</a>
-                                            <a class="dropdown-item" href="#">Another action</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="#">Something else here</a>
-                                        </div>
-                                    </div>
+                                    
                                 </div>
                                 <!-- Card Body -->
                                 <div class="reminders">
--- pages/admin.php
+++ pages/admin.php
@@ -303,49 +303,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['admin_id'])) {
                             <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-bell fa-fw"></i>
-                                <!-- Counter - Alerts -->
+                                
                                 <span class="badge badge-danger badge-counter">3+</span>
                             </a>
-                            <!-- Dropdown - Alerts -->
+                            
                             <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="alertsDropdown">
                                 <h6 class="dropdown-header">
                                     Alerts Center
                                 </h6>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
+                                <div class="dropdown-item d-flex align-items-center">
                                     <div class="mr-3">
                                         <div class="icon-circle bg-primary">
                                             <i class="fas fa-file-alt text-white"></i>
                                         </div>
                                     </div>
                                     <div>
-                                        <div class="small text-gray-500">December 12, 2019</div>
-                                        <span class="font-weight-bold">A new monthly report is ready to download!</span>
+                                        Hi <?php echo $user_data['fname'];?>, please don't forget to monitor which products are in low supply.
                                     </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
+                                </div>
+                                <div class="dropdown-item d-flex align-items-center">
                                     <div class="mr-3">
                                         <div class="icon-circle bg-success">
                                             <i class="fas fa-donate text-white"></i>
                                         </div>
                                     </div>
                                     <div>
-                                        <div class="small text-gray-500">December 7, 2019</div>
-                                        $290.29 has been deposited into your account!
+                                        New Arrival Alert: LED Headlights. Get 20% off our new LED headlights. Upgrade today!
                                     </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
+                                </div>
+                                <div class="dropdown-item d-flex align-items-center" href="#">
                                     <div class="mr-3">
                                         <div class="icon-circle bg-warning">
                                             <i class="fas fa-exclamation-triangle text-white"></i>
                                         </div>
                                     </div>
                                     <div>
-                                        <div class="small text-gray-500">December 2, 2019</div>
-                                        Spending Alert: We've noticed unusually high spending for your account.
+                                        All brake discs have passed quality checks. Sell with confidence!
                                     </div>
-                                </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                                </div>
+                                
                             </div>
                         </li>
 
